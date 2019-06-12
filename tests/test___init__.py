a5df6edd25540944f65207ffa0b9a1d1c51ae4a6
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-06-12 15:56:27 -0400

    Add better test in test_create_app
    The old test did not verify much. The test is updated with more
    verifications so that we know that default values and blueprints
    are added as they should.

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -6,12 +6,28 @@ from unittest.mock import MagicMock
 from flask import Flask
 
 import flaskr.__init__
+import flaskr.account
+import flaskr.dashboard
 import unittest
 
 
 class TestInit(unittest.TestCase):
     def test_create_app(self) -> None:
-        self.assertIsInstance(flaskr.__init__.create_app(), Flask)
+        flask = flaskr.__init__.create_app()
+
+        self.assertEqual(flask.config.get('SECRET_KEY'), 'dev')
+        self.assertEqual(flask.config.get('SERVICE_REPORT'), 'http://localhost:8080/mysql')
+        self.assertEqual(flask.config.get('SERVICE_VISUALIZATION'), 'http://localhost:8080/mysql')
+        self.assertEqual(flask.config.get('SESSION_TYPE'), 'filesystem')
+        self.assertEqual(flask.config.get('SMTP_FROM'), 'noreply@teacup.com')
+        self.assertEqual(flask.config.get('SMTP_HOST'), 'localhost')
+        self.assertEqual(flask.config.get('SMTP_PORT'), '1025')
+
+        blueprints = flask.blueprints
+
+        self.assertEqual(len(blueprints), 2)
+        self.assertIsNotNone(blueprints.get('account'))
+        self.assertIsNotNone(blueprints.get('dashboard'))
 
     def test_create_app_with_config(self) -> None:
         self.assertIsInstance(flaskr.__init__.create_app(MagicMock()), Flask)
